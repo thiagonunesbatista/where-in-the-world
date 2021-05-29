3c209f013dec026f1de096a9b0a7668d52e7af4d
Thiago Nunes Batista <423-thiago-nunes@users.noreply.git.conrad-caine.com>
2021-05-29 15:56:49 -0300

    :lipstick: update Container component styles

--- src/components/Container.js
+++ src/components/Container.js
@@ -8,6 +8,10 @@ const StyledContainer = styled.div`
   margin: 0 auto;
   width: 100%;
   max-width: 1190px;
+
+  @media (max-width: 375px) {
+    max-width: 343px;
+  }
 `
 
 export default Container
