be784a7047ce3c094cd9395aa88a2e32852db76d
Thiago Nunes Batista <423-thiago-nunes@users.noreply.git.conrad-caine.com>
2021-05-31 20:36:22 -0300

    :sparkles: add useClickOutside hook

---
+++ src/hooks/useClickOutside.js
@@ -0,0 +1,17 @@
+import { useEffect } from 'react'
+
+export const useClickOutside = (ref, handler) => {
+  useEffect(() => {
+    const handleClick = event => {
+      if (!ref.current && ref.current.contains(event.target)) {
+        return
+      }
+      handler(event)
+    }
+
+    document.addEventListener('click', handleClick)
+    return () => {
+      document.removeEventListener('click', handleClick)
+    }
+  }, [ref, handler])
+}
