1aa53eed69633355c941acf85c8689a675ccb348
Thiago Nunes Batista <423-thiago-nunes@users.noreply.git.conrad-caine.com>
2021-05-31 20:37:30 -0300

    :sparkles: add Select to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,21 @@
 import { useEffect, useState } from 'react'
+import styled from 'styled-components'
 
 import Container from 'components/Container'
-import { SearchInput } from 'components/forms'
+import { Select, SearchInput } from 'components/forms'
 import ListCountriesCards from 'components/ListCountriesCards'
 
 import CountriesClass from 'services/Countries'
 
 const CountriesService = new CountriesClass()
+
+const selectOptions = [
+  { label: 'Africa', value: 'Africa' },
+  { label: 'Americas', value: 'Americas' },
+  { label: 'Asia', value: 'Asia' },
+  { label: 'Europe', value: 'Europe' },
+  { label: 'Oceania', value: 'Oceania' }
+]
 
 const Home = () => {
   const [countries, setCountries] = useState([])
@@ -34,15 +43,43 @@ const Home = () => {
 
   return (
     <Container>
-      <SearchInput
-        placeholder='Search for a country...'
-        handleSubmit={handleSubmit}
-        handleInput={handleInput}
-        value={inputValue}
-      />
+      <FormContainer>
+        <SearchInput
+          placeholder='Search for a country...'
+          handleSubmit={handleSubmit}
+          handleInput={handleInput}
+          value={inputValue}
+        />
+
+        <Select
+          options={selectOptions}
+          headerMessage='Search for a country...'
+        />
+      </FormContainer>
+
       {countries.length > 0 && <ListCountriesCards countries={countries} />}
     </Container>
   )
 }
+
+const FormContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  margin: 40px 0;
+
+  @media (max-width: 768px) {
+    margin: 40px auto;
+    width: 343px;
+
+    & > *:first-child {
+      margin-bottom: 16px;
+    }
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`
 
 export default Home
