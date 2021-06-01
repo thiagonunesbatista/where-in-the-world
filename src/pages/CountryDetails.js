83bbcf7950a75a2b044390a4646a0da4df059a5e
Thiago Nunes Batista <423-thiago-nunes@users.noreply.git.conrad-caine.com>
2021-05-31 22:24:20 -0300

    :sparkles: finish CountryDetails component

--- src/pages/CountryDetails.js
+++ src/pages/CountryDetails.js
@@ -13,17 +13,57 @@ const CountryDetails = () => {
 
   useEffect(() => {
     const loadData = async () => {
-      const response = await CountriesService.getCountry(params.name)
-      setCountry(response)
+      const response = await CountriesService.searchCountry(params.name)
+      setCountry(response[0])
     }
     loadData()
   }, [params.name])
 
-  console.log(country)
+  if (!country) {
+    return <h2>Country not found</h2>
+  }
 
   return (
     <CountryDetailsContainer>
-      <img src={country?.flag} alt={`${country?.name} flag`} />
+      <img src={country.flag} alt={`${country?.name} flag`} />
+
+      <h2>{country.name}</h2>
+      <div>
+        <p>Native Name: {country.nativeName}</p>
+        <p>Population: {country.population}</p>
+        <p>Region: {country.region}</p>
+        <p>Sub Region: {country.subregion}</p>
+        <p>Capital: {country.capital}</p>
+      </div>
+
+      <div>
+        <p>
+          Top Love Domain:
+          {country.topLevelDomain.map(currentDomain => currentDomain)}
+        </p>
+        <p>Currencies: {country.currencies.map(currency => currency.name)}</p>
+        <p>
+          Languages:{' '}
+          {country.languages.map((language, index) => {
+            if (index < country.languages.length - 1) {
+              return `${language.name}, `
+            }
+            return `${language.name}.`
+          })}
+        </p>
+      </div>
+
+      <div>
+        <p>
+          Border Countries:{' '}
+          {country.borders.map((border, index) => {
+            if (index < country.borders.length - 1) {
+              return `${border}, `
+            }
+            return `${border}.`
+          })}
+        </p>
+      </div>
     </CountryDetailsContainer>
   )
 }
