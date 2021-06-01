abaff525a23cc7791051a5266423cfe5b6c9c400
Thiago Nunes Batista <423-thiago-nunes@users.noreply.git.conrad-caine.com>
2021-05-31 22:23:37 -0300

    :bug: fix useClickOutside hook

--- src/hooks/useClickOutside.js
+++ src/hooks/useClickOutside.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 export const useClickOutside = (ref, handler) => {
   useEffect(() => {
     const handleClick = event => {
-      if (!ref.current && ref.current.contains(event.target)) {
+      if (!ref.current && ref.current?.contains(event.target)) {
         return
       }
       handler(event)
